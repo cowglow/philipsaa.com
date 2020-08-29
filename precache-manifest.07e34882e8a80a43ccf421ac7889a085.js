ac843c04d1952987f7d6a82ddb675e899ca5326f
Philip Saa <cowglow@gmail.com>
2020-08-30 00:47:09 +0200

    Updates

---
+++ precache-manifest.07e34882e8a80a43ccf421ac7889a085.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fa566919bdbb2a754bd2a37170710f7",
+    "url": "/philipsaa.com/index.html"
+  },
+  {
+    "revision": "9ee6aa8981cd4e126988",
+    "url": "/philipsaa.com/static/css/main.19afbaa4.chunk.css"
+  },
+  {
+    "revision": "8c194489f4d5e7c32286",
+    "url": "/philipsaa.com/static/js/2.0100acdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/philipsaa.com/static/js/2.0100acdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ee6aa8981cd4e126988",
+    "url": "/philipsaa.com/static/js/main.6f4026dd.chunk.js"
+  },
+  {
+    "revision": "8ba759d97a827e9633f3",
+    "url": "/philipsaa.com/static/js/runtime-main.ba87c9d3.js"
+  },
+  {
+    "revision": "08cb8f79715774f9a6285ee7db2919a3",
+    "url": "/philipsaa.com/static/media/Roboto-Bold.08cb8f79.woff"
+  },
+  {
+    "revision": "f3a02e2578bee50e620e515912278bc9",
+    "url": "/philipsaa.com/static/media/Roboto-Bold.f3a02e25.woff2"
+  },
+  {
+    "revision": "94dac78eee406a8c8f0406b69b85ac2b",
+    "url": "/philipsaa.com/static/media/Roboto-Regular.94dac78e.woff"
+  },
+  {
+    "revision": "9feb0110b6dff9ee2b9ebd17f7a1aee6",
+    "url": "/philipsaa.com/static/media/Roboto-Regular.9feb0110.woff2"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/philipsaa.com/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b0697206ca374e755d074b3b90763da4",
+    "url": "/philipsaa.com/static/media/philipsaa.b0697206.jpg"
+  },
+  {
+    "revision": "eff10b509522e964a09b006922b18b8a",
+    "url": "/philipsaa.com/static/media/world.eff10b50.svg"
+  }
+]);
